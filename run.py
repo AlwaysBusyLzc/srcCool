c0209cb3a60719043aa103659145e32036062102
AlwaysBusyLzc <2035941342@qq.com>
2018-12-04 17:52:34 +0800

    提交多进程并行分析版本

--- run.py
+++ run.py
@@ -1,44 +1,37 @@
 # -*- coding: utf-8 -*-
 import time
-from multiprocessing.dummy import Pool
-
+from multiprocessing import Pool
 import getProjectLink
 import getProjectInfo
 
-
 keyWord = "python"
-
-#print(getProjectInfo.isHaveKeyWord("thread-1365-1-1.html", keyWord))
-
-
-for i in range(14):
-    print("page %d" % i)
-    getProjectLink.getOnePage(i)
-
-realNeedUrlList = []
 
 
 def dealOneProject(project):
     if getProjectInfo.isHaveKeyWord(str(project).split('="')[1], keyWord):
-        realNeedUrlList.append(getProjectInfo.projectUrl % str(project).split('="')[1])
+        return getProjectInfo.projectUrl % str(project).split('="')[1]
+    return ""
 
-for project in getProjectLink.projectList:
-    dealOneProject(project)
 
-def parseAll(processes=10):
+def parseAll(urlSet, processes=10):
     """ 并发分析 """
     start_time = time.time()
     pool = Pool(processes)
     for project in getProjectLink.projectList:
-        pool.apply_async(dealOneProject, (project, ))
+        urlSet.append(pool.apply_async(dealOneProject, args=(project,)))
 
     pool.close()
     pool.join()
+
     end_time = time.time()
     print('分析完毕,用时:%s秒' % (end_time - start_time))
 
 
-#parseAll(processes=10)
+if __name__ == '__main__':
+    for i in range(14):
+        print("page %d" % i)
+        getProjectLink.getOnePage(i)
 
-print(realNeedUrlList)
-
+    realNeedUrlSet = []
+    parseAll(realNeedUrlSet, processes=10)
+    print([x.get() for x in realNeedUrlSet if x.get() != ""])
